2275baf7606069d67f25036ea5c60eed3b674266
Ricardo <ricardokelebra@gmail.com>
2019-07-29 15:24:01 -0500

    Areas corregidas

--- src/components/Contenido/Contenido.js
+++ src/components/Contenido/Contenido.js
@@ -5,16 +5,16 @@ const Contenido = props => {
     return (
 
             <p>
-            {props.parrafoUno} <br/><br/> 
-            {props.parrafoDos}<br/><br/> 
-            {props.parrafoTres}<br/><br/> 
-            {props.parrafoCuatro} <br/><br/> 
-            {props.parrafoCinco} <br/><br/>
-            {props.parrafoSeis} <br/><br/>
-            {props.parrafoSiete}<br/><br/>
-            {props.parrafoOcho}<br/><br/>
-            {props.parrafoNueve}<br/><br/>
-            {props.parrafoDiez}<br/><br/>
+            {props.parrafoUno} <br/>
+            {props.parrafoDos} <br/>
+            {props.parrafoTres} <br/>
+            {props.parrafoCuatro} <br/>  
+            {props.parrafoCinco} 
+            {props.parrafoSeis} 
+            {props.parrafoSiete}
+            {props.parrafoOcho}
+            {props.parrafoNueve}
+            {props.parrafoDiez}
             {props.parrafoOnce}
             </p>
 
--- src/pages/Equipo.js
+++ src/pages/Equipo.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import '../css/equipo.css'
 
 const Equipo = props => {
-    return(
-        <h1>EquipoWorks!</h1>
+    return (
+        <div>
+            <div class="contenedorSeccion">
+                <p class="tituloSeccion">Equipo</p>
+            </div>
+            <main>
+                <section class="equipo">
+                    <div class="decorationWhiteRects">
+                        <div class="whiteDecorationRect">
+
+                            <div class="whiteDecorationRectIn"></div>
+                            <p>Raul Romero Trejo</p>
+                        </div>
+                        <div class="decorationRect">
+                            <a class="at-user__title" href="mailto:rromero@rasc.mx">rromero@rasc.mx</a>
+                        </div>
+                        <div class="whiteDecorationRect">
+                            <div class="whiteDecorationRectIn"></div>
+                            <p>Liliana Arenas Muñoz</p>
+                        </div>
+                        <div class="decorationRect">
+                            <a class="at-user__title" href="mailto:alarenas@rasc.mx">larenas@rasc.mx</a>
+                        </div>
+                        <div class="whiteDecorationRect">
+                            <div class="whiteDecorationRectIn"></div>
+                            <p>Armando Gutiérrez del Río</p>
+                        </div>
+                        <div class="decorationRect">
+                            <a class="at-user__title" href="mailto:agutierrez@rasc.mx">agutierrez@rasc.mx</a>
+                        </div>
+                        <div class="whiteDecorationRect">
+                            <div class="whiteDecorationRectIn"></div>
+                            <p>Arturo Corona Urióstegui</p>
+                        </div>
+                        <div class="decorationRect">
+                            <a class="at-user__title" href="mailto:acorona@rasc.mx">acorona@rasc.mx</a>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
 
-export default  Equipo;
+export default Equipo;
